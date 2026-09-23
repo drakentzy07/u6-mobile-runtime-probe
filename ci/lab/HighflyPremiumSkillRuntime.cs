3cba82b32febe7b1a75d0d381f2a056831408d99
drakentzy07 <lucaascabral07@gmail.com>
2026-09-23 16:36:21 -0300

    feat(lab): bypass premium skill cooldowns for rapid iteration

--- ci/lab/HighflyPremiumSkillRuntime.cs
+++ ci/lab/HighflyPremiumSkillRuntime.cs
@@ -196,6 +196,9 @@ namespace Highfly.SkillLab
 
         public void Trigger(HighflyPremiumSkillId id)
         {
+            if (HighflySkillLabMode.IsActive)
+                ClearPreviewCooldown(id);
+
             switch (id)
             {
                 case HighflyPremiumSkillId.TwinDance:
